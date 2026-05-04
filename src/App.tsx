f21e8aea84b0b7a9b0ebb00f54f88d5f51889012
lautaroboninom <124056629+lautaroboninom@users.noreply.github.com>
2026-05-04 10:38:00 -0300

    Use autosave admin wrapper

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Booking from './pages/Booking';
-import Admin from './pages/Admin';
+import AdminAutoSave from './pages/AdminAutoSave';
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/reservar/:serviceId" element={<Booking />} />
-      <Route path="/admin" element={<Admin />} />
+      <Route path="/admin" element={<AdminAutoSave />} />
     </Routes>
   );
 }
-
